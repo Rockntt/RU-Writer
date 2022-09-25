1738c8e4d910d5c6f93c57482d6272bd000d9171
Rockntt <m.klochkov@lnip.su>
2022-09-25 13:03:46 +0300

    V1.1

--- Compile.bat
+++ Compile.bat
@@ -1,5 +1,5 @@
 @echo off
-pyinstaller --noconfirm --onedir --console  "writer.py"
+pyinstaller --noconfirm --onefile --console --add-data "C:/Users/kloch/PycharmProjects/Writer/Fonts;Fonts/"  "C:\Users\kloch\PycharmProjects\Writer"
 
 rmdir /s /q __pycache__
 rmdir /s /q build
